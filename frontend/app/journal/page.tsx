1c87892a448baae31821a1d2d01b84e767b51c5c
ShreeyaKamath <shreeya.kamath12@gmail.com>
2026-07-05 21:48:56 +0530

    Update frontend/app/journal/page.tsx

--- frontend/app/journal/page.tsx
+++ frontend/app/journal/page.tsx
@@ -1,5 +1,6 @@
 import { JournalScreen } from "@/features/journal/journal-screen";
 
+/** Renders the Journal of Memories route. */
 export default function JournalPage() {
   return <JournalScreen />;
 }
